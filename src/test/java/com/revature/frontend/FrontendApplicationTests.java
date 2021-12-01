cdaf24b8bbf3e629c013f25672eaf7f8d387b8cc
Justin Sherfey <jsherfey505@gmail.com>
2021-12-01 07:43:52 -0800

    initialized project with spring initalized

---
+++ src/test/java/com/revature/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

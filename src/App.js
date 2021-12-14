b95a8cbfb2e33e9ad56f1577857809b94432b663
Isaiah Anason <isaiahsanason@gmail.com>
2021-12-14 10:42:02 -0800

    bareBones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RegisterMenu />
     </div>
   );
 }
+class MenuBar extends React.Component {
+  render() {
+    return (
+      <>
+        <a href="home.html">
+          <button>Homepage</button>
+        </a>
+      </>
+    );
+  }
+}
 
+function TestFunction() {
+  return <h1>someText</h1>;
+}
+class RegisterMenu extends React.Component {
+  render() {
+    return (
+      <>
+        <form id="registerForm">
+          <h1>Sign up</h1>
+
+          <p>Username:</p>
+          <input type="text" name="username" />
+          <br />
+
+          <p>Password:</p>
+          <input type="text" name="password" />
+          <br />
+
+          <input value="Submit" type="button" onclick="registerForm()" />
+        </form>
+      </>
+    );
+  }
+}
+class logginMenu extends React.Component {
+  render() {
+    return (
+      <>
+        <form id="loginForm">
+          <h1>Login</h1>
+          Username:
+          <input type="text" name="username" />
+          <br />
+          Password:
+          <input type="text" name="password" />
+          <br />
+          <input value="Submit" type="button" onclick="loginForm()" />
+        </form>
+      </>
+    );
+  }
+}
 export default App;

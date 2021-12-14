c9b89ecd9234825f8c1910a18cf2a8b9445fb595
nfrederick023 <nfrederick023@gmail.com>
2021-12-14 17:11:06 -0500

    Added Login

--- src/App.js
+++ src/App.js
@@ -1,75 +1,86 @@
-import logo from "./logo.svg";
 import React, { useState } from "react";
 import "./App.css";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
   return (
     <div>
       <NavigationBar />
-      <RegisterMenu />
+      <h1>Register:</h1>
+      <RegisterForm />
+      <h1>Login:</h1>
+      <LoginForm />
     </div>
   );
 }
+
 class NavigationBar extends React.Component {
   render() {
     return (
-      <>
-        <Navbar bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Amplifire RoadTrip</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#loginForm">Login</Nav.Link>
-              <Nav.Link href="#registerForm">Register</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Amplifire RoadTrip</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#loginForm">Login</Nav.Link>
+            <Nav.Link href="#registerForm">Register</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     );
   }
 }
 
-function TestFunction() {
-  return <h1>someText</h1>;
-}
-class RegisterMenu extends React.Component {
-  render() {
-    return (
-      <>
-        <form id="registerForm">
-          <h1>Sign up</h1>
+function RegisterForm() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => registerUser(JSON.stringify(data));
 
-          <p>Username:</p>
-          <input type="text" name="username" />
-          <br />
-
-          <p>Password:</p>
-          <input type="text" name="password" />
-          <br />
-
-          <Button varient="info">Submit</Button>
-        </form>
-      </>
-    );
+  const registerUser = (data) => {
+    axios.post(`http://localhost:5000/register`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => {
+        console.log(res);
+      })
   }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("username")} />
+      <input {...register("password")} />
+      <input type="submit" />
+    </form>
+  );
 }
-class logginMenu extends React.Component {
-  render() {
-    return (
-      <>
-        <form id="loginForm">
-          <h1>Login</h1>
-          Username:
-          <input type="text" name="username" />
-          <br />
-          Password:
-          <input type="text" name="password" />
-          <br />
-          <input value="Submit" type="button" onclick="loginForm()" />
-        </form>
-      </>
-    );
+
+function LoginForm() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => loginUser(JSON.stringify(data));
+
+  const loginUser = (data) => {
+    axios.post(`http://localhost:5000/login`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => {
+        sessionStorage['jwt'] = res.jwt;
+        console.log(res);
+      })
   }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("username")} />
+      <input {...register("password")} />
+      <input type="submit" />
+    </form>
+  );
 }
+
 export default App;

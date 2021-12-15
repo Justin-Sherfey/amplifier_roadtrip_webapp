b4aabbf74284b443f4b382e28981e9f286bf621f
nfrederick023 <nfrederick023@gmail.com>
2021-12-15 14:18:28 -0500

    Added Redirect and Improved Forms

--- src/App.js
+++ src/App.js
@@ -1,86 +1,72 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Form, Navbar, Container, Nav, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
+
+const urlConnection = "http://localhost:5000/"
+// const urlConnection = "http://amplifireroadtripbeanstalk-env.eba-amdewhu5.us-west-2.elasticbeanstalk.com/"
 
 function App() {
-  return (
 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavigationBar />}>
-          <Route path="HomePage" element={<HomePage />} />
-          <Route path="Login" element={<LoginMenu />} />
-          <Route path="Register" element={<RegisterMenu />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  function redirectIfloggedIn(privateRoute) {
+    return sessionStorage.getItem('jwt') ? <Navigate to="/Homepage" /> : privateRoute;
+  }
+
+  function redirectIfLoggedOut(privateRoute) {
+    return !sessionStorage.getItem('jwt') ? <Navigate to="/Login" /> : privateRoute;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route path="HomePage" element={<HomePage />} />
+        <Route path="Login" element={redirectIfloggedIn(<LoginForm />)} />
+        <Route path="Register" element={redirectIfloggedIn(<RegisterForm />)} />
+        <Route path="Account" element={redirectIfLoggedOut(<AccountForm />)} />
+      </Route >
+    </Routes >
   );
 }
 
-class NavigationBar extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand as={Link} to="HomePage">
-              Amplifire RoadTrip
-            </Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="Login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="Register">
-                Register
-              </Nav.Link>
-              <Link to="Register">Register</Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        <Outlet />
-      </>
-    );
-  }
-}
-class HomePage extends React.Component {
-  render() {
-    return (<><h1>Home</h1></>);
-  }
+function NavigationBar() {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="HomePage">
+            Amplifire RoadTrip
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="Login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="Register">
+              Register
+            </Nav.Link>
+            <Link to="Register">Register</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
 }
 
-class LoginMenu extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Login:</h1>
-        <LoginForm />
-      </>
-    );
-  }
+function HomePage() {
+  return (<><h1>Home</h1></>);
 }
 
-class RegisterMenu extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Register:</h1>
-        <RegisterForm />
-      </>
-    );
-  }
+function AccountForm() {
+  return (<><h1>Account</h1></>);
 }
-
 
 function RegisterForm() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => registerUser(JSON.stringify(data));
-  const registerUser = (data) => {
-    axios.post(`http://localhost:5000/register`, data, {
+  const registerUser = data => {
+    axios.post(urlConnection + "register", JSON.stringify(data), {
       headers: {
         'Content-Type': 'application/json',
       }
@@ -91,35 +77,43 @@ function RegisterForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("username")} />
-      <input {...register("password")} />
-      <input type="submit" />
-    </form>
+    <div>
+      <h1>Register:</h1>
+      <Form onSubmit={handleSubmit(registerUser)}>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control {...register("username")}></Form.Control>
+        <Form.Label>Password:</Form.Label>
+        <Form.Control {...register("password")}></Form.Control>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </div>
   );
 }
 
 function LoginForm() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => loginUser(JSON.stringify(data));
-  const loginUser = (data) => {
-    axios.post(`http://localhost:5000/login`, data, {
+  const loginUser = data => {
+    axios.post(urlConnection + "login", JSON.stringify(data), {
       headers: {
         'Content-Type': 'application/json',
       }
     })
       .then(res => {
-        sessionStorage['jwt'] = res.jwt;
-        console.log(res);
+        sessionStorage['jwt'] = res.data.jwt;
       })
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("username")} />
-      <input {...register("password")} />
-      <input type="submit" />
-    </form>
+    <div>
+      <h1>Login:</h1>
+      <Form onSubmit={handleSubmit(loginUser)}>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control {...register("username")}></Form.Control>
+        <Form.Label>Password:</Form.Label>
+        <Form.Control {...register("password")}></Form.Control>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </div>
   );
 }
 
